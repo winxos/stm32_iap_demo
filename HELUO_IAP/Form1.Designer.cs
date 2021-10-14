8c50908a171b200b29c6f54d8d6bc92852f502b3
wvv <winxos@xhu.edu.cn>
2021-10-14 16:09:40 +0800

    添加项目文件。

---
+++ HELUO_IAP/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace HELUO_IAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(174, 36);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(183, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "连接设备";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.Gainsboro;
+            this.comboBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(3, 53);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(174, 36);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Location = new System.Drawing.Point(3, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "加载本地固件";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.Location = new System.Drawing.Point(183, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "获取远程固件";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 253);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(701, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(363, 3);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.tableLayoutPanel1.SetRowSpan(this.listView1, 4);
+            this.listView1.Size = new System.Drawing.Size(335, 247);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.VirtualMode = true;
+            this.listView1.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.ListView1_RetrieveVirtualItem);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(701, 253);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 153);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 97);
+            this.textBox1.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.checkBox1.Location = new System.Drawing.Point(183, 103);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(174, 44);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "自动升级";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(701, 275);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ HELUO_IAP/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace HELUO_IAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        const string server_version_address = "https://raw.githubusercontent.com/winxos/files/master/config.json";
+        class FirmWare
+        {
+            public byte[] data;
+            public byte block_size;
+            public byte sending_index;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "固件|*.bin";
+            openFileDialog1.Title = "请选择固件进行升级";
+            DialogResult d= openFileDialog1.ShowDialog();
+            if(d==DialogResult.OK)
+            {
+                FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                BinaryReader binReader = new BinaryReader(fs);
+                _fw.data = new byte[fs.Length];
+                binReader.Read(_fw.data, 0, (int)fs.Length);
+                binReader.Close();
+                fs.Close();
+                _fw.sending_index = 0;
+                _fw.block_size = 200;
+                toolStripStatusLabel1.Text = "加载固件" + openFileDialog1.FileName;
+                textBox1.Text = String.Format("固件名称：{0}\r\n固件大小：{1} Bytes",Path.GetFileName(openFileDialog1.FileName), _fw.data.Length);
+            }
+
+        }
+        enum CMD
+        {
+            INFO=0xf0,DATA,TIMEOUT,FINISHED
+        }
+        void send_bytes(byte[] ds)
+        {
+
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(ds, 0, ds.Length);
+            }
+            else
+            {
+                MessageBox.Show("请先连接串口");
+            }
+        }
+        Thread rec;
+        private List<ListViewItem> _out_msg;
+        const int idle_tick = 3;
+        bool is_ticking = false;
+        void uart_check()
+        {
+            int last_received_timeout = 0;
+            List<byte> frame = new List<byte>();
+            while (true)
+            {
+                while (serialPort1.BytesToRead > 0)
+                {
+                    if (is_ticking == false)
+                    {
+                        is_ticking = true;
+                        frame.Clear();//数据上升沿
+                    }
+                    frame.Add((byte)serialPort1.ReadByte());
+                    last_received_timeout = 0;
+                }
+                if (is_ticking)
+                {
+                    last_received_timeout++;
+                    if (last_received_timeout > idle_tick)
+                    {
+                        //idle callback
+                        uart_deal(frame.ToArray());
+                        is_ticking = false;
+                    }
+                }
+                Thread.Sleep(10); //因为windows 调度时间片关系，太小精度意义不大
+            }
+        }
+        void uart_deal(byte[] lb)
+        {
+
+            if(calc_check(lb,lb.Length-1)==lb[lb.Length-1])
+            {
+                string ss = "";
+                for (int i = 0; i < lb.Length; i++)
+                {
+                    ss += lb[i].ToString("X2") + " ";
+                }
+                this.Invoke(new Action(() =>
+                {
+                    _out_msg.Add(new ListViewItem(new string[] { _out_msg.Count.ToString(), ss }));
+                    listView1.VirtualListSize = _out_msg.Count;
+                    listView1.EnsureVisible(listView1.Items.Count - 1);
+                }));
+                if(lb[0]==0xf0)
+                {
+                    if(checkBox1.Checked)
+                    {
+                        _fw.sending_index = 0;
+                        _iapstate = IapState.SEND_INFO;
+                    }
+                    
+                }
+                else if(lb[0]==0xf1)
+                {
+                    if(_iapstate==IapState.WAITING)
+                    {
+                        _iapstate = IapState.SENDING;
+                    }
+                }
+            }
+        }
+
+        FirmWare _fw = new FirmWare();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "固件升级工具 成都合洛科技 v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            string[] n = SerialPort.GetPortNames();
+            foreach (string s in n)
+            {
+                comboBox1.Items.Add(s);
+            }
+            comboBox1.SelectedIndex = comboBox1.Items.Count-1;
+            toolStripStatusLabel1.Text = "请先连接设备，然后加载固件";
+            _out_msg = new List<ListViewItem>();
+            listView1.VirtualListSize = 0;
+            listView1.Columns.Add(new ColumnHeader() { Text = "ID", Width = 40 });
+            listView1.Columns.Add(new ColumnHeader() { Text = "DATA" });
+            listView1.Columns[1].Width = listView1.ClientSize.Width - listView1.Columns[0].Width - 30;
+            new Thread(iap_loop).Start();
+        }
+        byte calc_check(byte[] data, int len)
+        {
+            int n = 0;
+            for (int i = 0; i < len; i++)
+            {
+                n += data[i];
+            }
+            n = n % 256;
+            return Convert.ToByte(255 - n);
+        }
+        byte[] _build_frame(byte cmd, byte[] data)
+        {
+            byte[] _tx_buf;
+            if (data == null)
+            {
+                _tx_buf = new byte[2];
+            }
+            else
+            {
+                _tx_buf = new byte[data.Length + 2];
+                data.CopyTo(_tx_buf, 1);
+            }
+            _tx_buf[0] = cmd;
+            _tx_buf[_tx_buf.Length - 1] = calc_check(_tx_buf, _tx_buf.Length - 1);
+            return _tx_buf;
+        }
+        bool frame_valid(byte[]v)
+        {
+            return calc_check(v,v.Length-1) == v[v.Length - 1];
+        }
+        enum IapState
+        {
+            IDLE,SEND_INFO,SENDING, WAITING,TIMEOUT,FINISHED
+        }
+        void send_info()
+        {
+            byte[] tx = new byte[4];
+            tx[0] = _fw.block_size;
+            int t = _fw.data.Length;
+            tx[3] = (byte)(t&0xff);
+            t = t >> 8;
+            tx[2] = (byte)(t & 0xff);
+            t = t >> 8;
+            tx[1] = (byte)(t & 0xff);
+            byte[] b = _build_frame((byte)CMD.INFO, tx);
+            send_bytes(b);
+        }
+        void send_finished()
+        {
+            byte[] b = _build_frame((byte)CMD.FINISHED, null);
+            send_bytes(b);
+        }
+        IapState _iapstate = IapState.IDLE;
+        const int TICK_TIMEOUT = 1000000;
+        void iap_loop()
+        {
+            byte []tx;
+            int tick = 0;
+            while(true)
+            {
+                switch(_iapstate)
+                {
+                    case IapState.IDLE:
+
+                        break;
+                    case IapState.SEND_INFO:
+                        send_info();
+                        _iapstate = IapState.WAITING;
+                        break;
+                    case IapState.SENDING:
+                        if(_fw.sending_index==(_fw.data.Length/_fw.block_size))
+                        {
+                            int remain = _fw.data.Length - _fw.block_size * _fw.sending_index;
+                            tx = new byte[remain];
+                            Array.Copy(_fw.data, _fw.sending_index * _fw.block_size, tx, 0, remain);
+                            _iapstate = IapState.FINISHED;
+                            _fw.sending_index = 0;
+                        }
+                        else
+                        {
+                            tx = new byte[_fw.block_size];
+                            Array.Copy(_fw.data, _fw.sending_index * _fw.block_size, tx, 0, _fw.block_size);
+                            _iapstate = IapState.WAITING;
+                        }
+                        byte[] b = _build_frame(0xf1, tx);
+                        send_bytes(b);
+                        _fw.sending_index++;
+                        tick = 0;
+                        this.Invoke(new Action(() =>
+                        {
+                            toolStripStatusLabel1.Text = String.Format("升级中{0,4}/{1,4} 请勿断开设备电源！", _fw.sending_index, _fw.data.Length / _fw.block_size);
+                        }));
+                        break;
+                    case IapState.WAITING:
+                        tick++;
+                        if (tick > TICK_TIMEOUT)
+                        {
+                            _iapstate = IapState.TIMEOUT;
+                        }
+                        break;
+                    case IapState.TIMEOUT:
+                        this.Invoke(new Action(() =>
+                        {
+                            toolStripStatusLabel1.Text = String.Format("设备无响应，请检查设备连线！");
+                        }));
+                        break;
+                    case IapState.FINISHED:
+                        Thread.Sleep(100);
+                        this.Invoke(new Action(() =>
+                        {
+                            toolStripStatusLabel1.Text = String.Format("升级完成，请重启设备！");
+                        }));
+                        send_finished();
+                        _iapstate = IapState.IDLE;
+                        break;
+                }
+                Thread.Sleep(5);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "连接设备")
+            {
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.BaudRate = 115200;
+                serialPort1.Encoding = Encoding.UTF8;
+                serialPort1.Open();
+                button1.Text = "断开设备";
+                toolStripStatusLabel1.Text = "设备已连接";
+                rec = new Thread(uart_check);
+                rec.Start();
+            }
+            else
+            {
+                rec.Abort();
+                serialPort1.Close();
+                button1.Text = "连接设备";
+                toolStripStatusLabel1.Text = "设备已断开";
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListView1_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            e.Item = _out_msg[e.ItemIndex];
+        }
+    }
+}
